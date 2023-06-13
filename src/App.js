adf90a6eb8c1fe4734b84285a6f3bd0aa80ed9dd
Mubasher Ahmad <mubasherahmad117@gmail.com>
2023-06-13 23:08:56 +0500

    iniit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+        <div style={{display: 'flex', justifyContent: 'center', paddingTop: 40}}>
+          <img src={require('./card.jpeg')} style={{width: "90%"}} alt="wakeel360" />
+        </div>
+       
+  );
+}
+
+export default App;
